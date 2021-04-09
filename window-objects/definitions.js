c5ba0f33a52cfcac9d71a8db956b26f01ae17332
Jason Edwards <Jason.Edwards@riskonnect.com>
2021-04-09 12:45:29 -0500

    Add files

---
+++ window-objects/definitions.js
@@ -0,0 +1,8 @@
+Object.defineProperty(window, 'PointerEvent', {
+    value: class PointerEvent {}
+  });
+  
+  Object.defineProperty(window, 'DragEvent', {
+    value: class DragEvent {}
+  });
+  
