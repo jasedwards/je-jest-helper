9a4c084f9c8ed47721008157046516b08467d529
Jason Edwards <Jason.Edwards@riskonnect.com>
2021-04-27 18:46:13 -0500

    fix(windowObject) Add matchMedia

--- window-objects/definitions.js
+++ window-objects/definitions.js
@@ -5,4 +5,17 @@ Object.defineProperty(window, 'PointerEvent', {
   Object.defineProperty(window, 'DragEvent', {
     value: class DragEvent {}
   });
-  
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
